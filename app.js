5f94b87121cc4b2b735c0f03b55f811b38c55bd4
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2023-09-14 17:45:38 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var countdownDate = new Date("Sep 20, 2023 00:00:00").getTime()
+var x = setInterval(function(){
+  var now = new Date().getTime()
+  var distance = countdownDate - now
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ 
+document.querySelector('.days').innerHTML = days
+document.querySelector('.hours').innerHTML = hours
+document.querySelector('.minutes').innerHTML = minutes
+document.querySelector('.seconds').innerHTML = seconds
+
+if(distance < 0){
+  clearInterval()
+
+
+}
+
+
+
+
+
+
+},1000)
